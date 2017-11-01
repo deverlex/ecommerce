3fbc392fa54b2510b33b2891164ae332cfcd36bd
Spider man <nguyendo94vn@gmail.com>
2017-11-01 10:12:30 +0700

    enable cache using redis

--- src/main/java/vn/needy/ecommerce/configuration/ApplicationDatabaseConfiguration.java
+++ src/main/java/vn/needy/ecommerce/configuration/ApplicationDatabaseConfiguration.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -136,6 +138,14 @@ public class ApplicationDatabaseConfiguration {
 		template.setHashValueSerializer(new GenericToStringSerializer<Object>(Object.class));
 		template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
 		return template;
+	}
+	
+	@Bean("RedisCacheManager")
+	public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
+	    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+	    // Number of seconds before expiration. Defaults to unlimited (0)
+	    cacheManager.setDefaultExpiration(300);
+	    return cacheManager;
 	}
 	
     /// Security filter
