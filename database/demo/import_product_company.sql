9cadaaf382b87ea25a8705560473db3384f33bd9
Spider man <nguyendo94vn@gmail.com>
2017-11-03 18:05:02 +0700

    add import demo data.

---
+++ database/demo/import_product_company.sql
@@ -0,0 +1,5 @@
+INSERT INTO ProductCompany(`productId`, `companyId`, `quantity`, `price`, `prePrice`, `lastUpdatedBy`)
+VALUES
+(1, 2, 5, 280000, 280000, 1),
+(2, 2, 6, 290000, 280000, 1),
+(3, 2, 7, 55000, 55000, 1);
---
+++ database/demo/import_products.sql
@@ -0,0 +1,5 @@
+INSERT INTO Products (`productNumber`, `category`, `state`, `name`, `lastUpdatedBy`)
+VALUES
+("12531ABDS", "GAS", 1, "Gas Hồng Hà", 1),
+("14521ABDS", "GAS", 1, "Gas Petro", 1),
+("15521ABFS", "WATER", 1, "Nước lavie", 1); 
