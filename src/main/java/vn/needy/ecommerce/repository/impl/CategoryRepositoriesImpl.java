1559cb82d89a4b0f5410b2691154f5c1ca30c135
Deverlex <nguyendo94vn@gmail.com>
2017-12-06 11:42:06 +0700

    increase method get categories list.

--- src/main/java/vn/needy/ecommerce/repository/impl/CategoryRepositoriesImpl.java
+++ src/main/java/vn/needy/ecommerce/repository/impl/CategoryRepositoriesImpl.java
@@ -16,49 +16,43 @@ public class CategoryRepositoriesImpl implements CategoryRepository {
 
 	@Autowired
 	JdbcTemplate jdbc;
-	
 
 	@Override
 	public List<Category> getLinkCategories(String preCategory) {
-		SqlRowSet rs = jdbc.queryForRowSet("select lc.category_name " +
-						"	from category cat " +
-						"	inner join link_category lc on lc.reference_name = cat.name " +
-						"	inner join category sca on lc.category_name = sca.name " +
-						"where cat.name = ? and cat.enable = 1 " +
-						"and lc.is_next = 1 and sca.enable = 1",
+		SqlRowSet rs = jdbc.queryForRowSet("select category_name " +
+						"from link_category " +
+						"where reference_name = ? " +
+						"and is_next = 1",
 					new Object[] {preCategory}); 
 	    List<Category> categories = new LinkedList<>(); 
 	    while(rs.next()) {
-	      Category subCategory = new Category(); 
-	      subCategory.setName(rs.getString("category_name"));
-	      categories.add(subCategory); 
+	      Category category = new Category();
+	      category.setName(rs.getString("category_name"));
+	      categories.add(category);
 	    }
 	    return categories;
 	}
 
 	@Override
 	public List<Category> getCompanyLinkCategories(long companyId, String category) {
-		SqlRowSet rs = jdbc.queryForRowSet("select cat.name from (" +
+		SqlRowSet rs = jdbc.queryForRowSet("select rt.category_name from " +
+				"(" +
 				"	select distinct lc.reference_name as reference_name from company c" +
 				"	inner join product p on p.company_id = c.id" +
-				"	inner join category cat on cat.name = p.category_name" +
-				"	inner join link_category lc on lc.category_name = cat.name" +
-				"	where c.id = ? and cat.enable = 1" +
+				"	inner join link_category lc on lc.category_name = p.category_name" +
+				"	where c.id = ?" +
 				") as ct " +
 				"inner join " +
 				"(" +
-				"	select distinct lc.category_name as category_name, lc.is_next as is_next " +
-				"	from category cat" +
-				"   inner join link_category lc on lc.reference_name = cat.name" +
-				"   where cat.name = ? and cat.enable = 1" +
+				"	select distinct category_name" +
+				"   from link_category" +
+				"   where reference_name = ? and is_next = 1" +
 				") as rt " +
-				"on ct.reference_name = rt.category_name " +
-				"inner join category cat on rt.category_name = cat.name " +
-				"where rt.is_next = 1 and cat.enable = 1", new Object[] {companyId, category});
+				"on rt.category_name = ct.reference_name", new Object[] {companyId, category});
 		List<Category> categories = new LinkedList<>();
 		while(rs.next()) {
 			Category cat = new Category();
-			cat.setName(rs.getString("name"));
+			cat.setName(rs.getString("category_name"));
 			categories.add(cat);
 		}
 		return categories;
