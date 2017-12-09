6e9ac55cdcc1b6612550de93896eef2430a228d0
Deverlex <nguyendo94vn@gmail.com>
2017-12-09 08:52:42 +0700

    add response code, company defined.

---
+++ src/main/java/vn/needy/ecommerce/api/base/ResponseCode.java
@@ -0,0 +1,26 @@
+package vn.needy.ecommerce.api.base;
+
+public enum  ResponseCode {
+
+    NOT_FOUND(1),
+    EXIST(2);
+
+    private int code;
+
+    private ResponseCode(final int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(code);
+    }
+
+    public String getName() {
+        return this.getName();
+    }
+}
